dabcef0f8a5aabd2cbe1976b906e479c7411bba6
gealit <gealit0369@yandex.ru>
2022-10-14 14:21:53 +0300

    added messages, and some styling

--- board/views.py
+++ board/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.http import Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import FormView, ListView, CreateView, DeleteView, UpdateView
 
@@ -12,16 +12,28 @@ from board.models import BoardMessage
 
 
 class LoginPage(LoginView):
+    """Simple login view which renders login page"""
     form_class = CustomLoginForm
     template_name = 'board/login.html'
     redirect_authenticated_user = True
     next_page = '/'
 
+    def get_context_data(self, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.success(self.request, 'Для входа введите ваш логин и пароль.')
+        return super().get_context_data(**kwargs)
+
 
 class RegisterPage(FormView):
+    """Users registration"""
     template_name = 'board/register.html'
     form_class = RegisterForm
     success_url = reverse_lazy('register_teacher')
+
+    def get_context_data(self, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.success(self.request, 'Для регистрации заполните форму ниже')
+        return super().get_context_data(**kwargs)
 
     def form_valid(self, form):
         user = form.save()
@@ -29,7 +41,7 @@ class RegisterPage(FormView):
         user.first_name = form.cleaned_data['first_name']
         user.last_name = form.cleaned_data['last_name']
         user.save()
-        messages.success(self.request, f'Пользователь {user} зарегистрирован!')
+        messages.success(self.request, f'Добро пожаловать, {user}! Вы зарегистрированы!')
         if user is not None:
             login(self.request, user)
             return redirect('board')
@@ -37,11 +49,13 @@ class RegisterPage(FormView):
 
 
 class BoardListView(LoginRequiredMixin, ListView):
+    """Shows all the post on the main page, only for registered and authenticated users."""
     model = BoardMessage
     template_name = 'board/board.html'
 
 
 class BoardMessageCreateView(LoginRequiredMixin, CreateView):
+    """Creation the post for the board"""
     model = BoardMessage
     form_class = BoardMessageCreateForm
     template_name = 'board/boardmessage_create.html'
@@ -49,14 +63,20 @@ class BoardMessageCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, f'Пост {form.instance.title} добавлен')
         return super(BoardMessageCreateView, self).form_valid(form)
 
 
 class BoardMessageUpdateView(LoginRequiredMixin, UpdateView):
+    """Update your posts. Only you will be able to do it."""
     model = BoardMessage
     form_class = BoardMessageCreateForm
     template_name = 'board/boardmessage_update.html'
     success_url = reverse_lazy('board')
+
+    def form_valid(self, form):
+        messages.success(self.request, f'Пост {form.instance.title} обновлен')
+        return super(BoardMessageUpdateView, self).form_valid(form)
 
     def get_object(self, queryset=None):
         """ Hook to ensure object is owned by request.user. """
@@ -72,9 +92,14 @@ class BoardMessageUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class BoardMessageDeleteView(LoginRequiredMixin, DeleteView):
+    """Delete your posts. Only you will be able to do it."""
     model = BoardMessage
     template_name = 'board/delete.html'
     success_url = reverse_lazy('board')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Ваш пост успешно удален!')
+        return super().form_valid(form)
 
     def get_object(self, queryset=None):
         """ Hook to ensure object is owned by request.user. """
